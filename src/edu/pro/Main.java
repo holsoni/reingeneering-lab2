ede74e50831369e3d06ee7a4b86ccc0507710962
holsoni <soniakostashchuk@gmail.com>
2024-03-06 15:26:39 +0200

    Refactor

--- src/edu/pro/Main.java
+++ src/edu/pro/Main.java
@@ -1,71 +1,89 @@
 package edu.pro;
 
+/*
+    @author   soniakk
+    @class  Main
+    @version  1.0
+    @project   reingeneering-lab2
+*/
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Locale;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
-
-    public static String cleanText(String url) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(url)));
-        content = content.replaceAll("[^A-Za-z ]"," ").toLowerCase(Locale.ROOT);
-        return content;
-    }
+    private static final String SPACE = " ";
+    private static final String EMPTY_STRING = "";
+    private static final String NON_ALPHABETIC_PATTERN = "[^A-Za-z ]";
+    private static final String NON_NUMERIC_PATTERN = "[^0-9]";
+    private static final String FILE_PATH = "src/edu/pro/txt/harry.txt";
+    private static final int SHOW_LIMIT = 30;
 
     public static void main(String[] args) throws IOException {
 
         LocalDateTime start = LocalDateTime.now();
-       // Path path = Paths.get()
-        String content = new String(Files.readAllBytes(Paths.get("src/edu/pro/txt/harry.txt")));
 
-        content = content.replaceAll("[^A-Za-z ]"," ").toLowerCase(Locale.ROOT);
+        String content = cleanText(FILE_PATH);
 
-        String[] words = content.split(" +"); // 400 000
+        String[] words = content.split(SPACE);
 
-        Arrays.sort(words);
+        Map<String, Long> wordsAmount = Arrays.stream(words)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        String distinctString = " ";
+        String[] distincts = wordsAmount.entrySet().stream()
+                .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
+                .limit(30)
+                .map(entry -> entry.getKey() + EMPTY_STRING + entry.getValue())
+                .toArray(String[]::new);
 
-        for (int i = 0; i < words.length ; i++) {
-            if (!distinctString.contains(words[i])){
-                distinctString+= words[i] + " ";
+        //This is another possible way to do this
+
+        /* Arrays.sort(words);
+        StringBuilder distinctString = new StringBuilder(SPACE);
+
+        for (String word : words) {
+            if (!distinctString.toString().contains(word)) {
+                distinctString.append(word).append(SPACE);
             }
         }
 
-        String[] distincts = distinctString.split(" ");
-        int[] freq = new int[distincts.length];
+        String[] distinctWords = distinctString.toString().split(SPACE);
+        int[] freq = new int[distinctWords.length];
 
-        for (int i = 0; i < distincts.length ; i++) {
+        for (int i = 0; i < distinctWords.length ; i++) {
             int count = 0;
-            for (int j = 0; j < words.length ; j++) {
-                if (distincts[i].equals(words[j])) {
+            for (String word : words) {
+                if (distinctWords[i].equals(word)) {
                     count++;
                 }
             }
             freq[i] = count;
         }
 
-        for (int i = 0; i < distincts.length ; i++) { // 5 000
-            distincts[i] += " " + freq[i];
+        for (int i = 0; i < distinctWords.length ; i++) { // 5 000
+            distinctWords[i] += SPACE + freq[i];
         }
 
-        Arrays.sort(distincts, Comparator.comparing(str
-                -> Integer.valueOf(str.replaceAll("[^0-9]", ""))));
+        Arrays.sort(distinctWords, Comparator.comparing(str
+                -> Integer.valueOf(str.replaceAll(NON_NUMERIC_PATTERN, EMPTY_STRING))));*/
 
-        for (int i = 0; i < 30; i++) {
-            System.out.println(distincts[distincts.length - 1 - i]);
+        for (int i = 0; i < SHOW_LIMIT; i++) {
+            System.out.println(distincts[i]);
         }
         LocalDateTime finish = LocalDateTime.now();
 
         System.out.println("------");
         System.out.println(ChronoUnit.MILLIS.between(start, finish));
+    }
 
+    private static String cleanText(String url) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(url)));
+        content = content.replaceAll(NON_ALPHABETIC_PATTERN, SPACE).toLowerCase(Locale.ROOT);
+        return content;
     }
 }
